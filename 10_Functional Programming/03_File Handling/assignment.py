1d441e0ccbfde6da6cd65379abf3e2313d307f00
Kevin Lloyd <Lloydkev000@warren.k12.in.us>
2024-03-21 12:19:30 -0400

    update

--- 10_Functional Programming/03_File Handling/assignment.py
+++ 10_Functional Programming/03_File Handling/assignment.py
@@ -1,10 +1,9 @@
-file_path = "C:\\Users\\Nicholas Armstrong\\Development\\POC_Sem2_Assignments\\10_Functional Programming\\03_File Handling\\assignment.py"
+file_path = "c:\\Users\lloydkev000\\POC_Sem2_Assignments\\10_Functional Programming\\03_File Handling\\myFile.txt"
 
 try:
     stream = open(file_path)
-    #YOUDO print to console using stream.read()
-    #YOUDO don't forget to close the stream
+    print(stream.read())
+    stream.close()
 except:
     pass
-##YOUDO rest of the program for reading and printing to the console
-##remember pass in file_path to open 
+ 
