689b02c398747a4e37c4e9166bb8e77b432f6b9b
Kevin Lloyd <Lloydkev000@warren.k12.in.us>
2024-05-15 11:46:06 -0400

    update

--- 12_GameDesign_1/connect_four_graphical.py
+++ 12_GameDesign_1/connect_four_graphical.py
@@ -1,15 +1,16 @@
-from tkinter import*
+from tkinter import *
 
 root = Tk()
 
-grid_color = "003AFF"
-red_color = "fF1800"
-yellow_color = "FFF600"
+grid_color = "#003AFF"
+red_color = "#FF1800"
+yellow_color = "#FFF600"
 grid_width = 420
-grid_height = 420
-diameter = grid_width/7
-total_row = 6
-total_col = 7
+grid_height = grid_width
+diameter = grid_width / 7
+col_width = diameter
+total_rows = 6
+total_cols = 7
 is_game_over = False
 current_col_choice = -1
 piece_placed = False
@@ -19,15 +20,16 @@ screen.pack()
 
 
 grid = [
-    ["-", "-", "-", "-", "-", "-", "-"],
+    ["-", "-", "-", "-", "-", "-", "-"],     # [R0C0, ..., R0C6]
     ["-", "-", "-", "-", "-", "-", "-"],     # [R1C0, ..., R1C6]
     ["-", "-", "-", "-", "-", "-", "-"],     # [R2C0, ..., R2C6]
     ["-", "-", "-", "-", "-", "-", "-"],     # [R3C0, ..., R3C6]
     ["-", "-", "-", "-", "-", "-", "-"],     # [R4C0, ..., R4C6]
-    ["-", "-", "-", "-", "-", "-", "-"]     
+    ["-", "-", "-", "-", "-", "-", "-"]      # [R5C0, ..., R5C6]
 ]
 
 current_piece = "R"
+
 last_row = -1
 last_col = -1
 remaining_spots = 42
@@ -40,41 +42,43 @@ def draw_circle(row, col, color):
     screen.create_oval(x_start, y_start, x_end, y_end, fill=color)
     pass
 
-def draw_grid():
-    for row in range(total_row):
-        for col in range(total_col):
-             draw_circle(row, col, "FFFFFF")
 
-def is_bad_num_string(choice : str):
+def draw_grid():
+    for row in range(total_rows):
+        for col in range(total_cols):
+            draw_circle(row, col, "#FFFFFF")  
+
+def is_bad_num_string(choice: str):
     if (choice.isnumeric() and int(choice) >= 0 and int(choice) <= 6):
         return False
     return True
-                
-def is_bad_choice(choice : str):
-    if(choice.__eq__("STOP")):
+
+
+def is_bad_choice(choice: str):
+    if (choice.__eq__("STOP")):
         return False
     return is_bad_num_string(choice)
 
-    
-def place_piece(col : int):
-     global last_row
-     global last_col
-     global remaining_spots
-     global current_col_choice
-     global piece_placed
-     while(True):
+
+def place_piece():
+    global last_row
+    global last_col
+    global remaining_spots
+    global current_col_choice
+    global piece_placed
+    while (True):
         row = 5
-        while(row >= 0):
+        while (row >= 0):
             if grid[row][current_col_choice].__eq__("-"):
                 grid[row][current_col_choice] = current_piece
                 last_row = row
                 last_col = current_col_choice
-                remaining_spots -=1
+                remaining_spots -= 1
                 piece_placed = True
                 if current_piece.__eq__("R"):
-                    draw_circle(row, col, red_color)
+                    draw_circle(row, current_col_choice, red_color)
                 else:
-                        draw_circle(row, col, yellow_color)
+                    draw_circle(row, current_col_choice, yellow_color)
                 break
             else:
                 row -= 1
@@ -83,49 +87,44 @@ def place_piece(col : int):
         else:
             piece_placed = False
             break
-            user_choice = ""
-            while ( is_bad_num_string(user_choice)):
-                user_choice = input("Enter a number (0-6) where to drop the piece:")
-                col = int(user_choice)
+
 
 
 def check_row():
     first_list = [last_col, last_col + 1, last_col + 2, last_col + 3]
-    second_list = [last_col - 1, last_col, last_col , last_col + 2]
-    third_list = [last_col - 2, last_col - 1, last_col , last_col + 1]
-    fourth_list = [last_col - 3, last_col - 2, last_col - 1 , last_col]
-    if(first_list[ 0] >=0 and first_list[0] <7 and first_list[3] >= 0 and first_list[3 < 7]):
+    second_list = [last_col - 1, last_col, last_col + 1, last_col + 2]
+    third_list = [last_col - 2, last_col - 1, last_col, last_col + 1]
+    fourth_list = [last_col - 3, last_col - 2, last_col - 1, last_col]
+    if(first_list[0] >= 0 and first_list[0] < 7 and first_list[3] >= 0 and first_list[3] < 7):
         one = grid[last_row][first_list[0]]
         two = grid[last_row][first_list[1]]
         three = grid[last_row][first_list[2]]
         four = grid[last_row][first_list[3]]
         if one.__eq__(two) and two.__eq__(three) and three.__eq__(four):
             return True
-
-    if(second_list[ 0] >=0 and first_list[0] <7 and first_list[3] >= 0 and first_list[3 < 7]):
-        one = grid[last_row][first_list[0]]
-        two = grid[last_row][first_list[1]]
-        three = grid[last_row][first_list[2]]
-        four = grid[last_row][first_list[3]]
+    if (second_list[0] >= 0 and second_list[0] < 7 and second_list[3] >= 0 and second_list[3] < 7):
+        one = grid[last_row][second_list[0]]
+        two = grid[last_row][second_list[1]]
+        three = grid[last_row][second_list[2]]
+        four = grid[last_row][second_list[3]]
         if one.__eq__(two) and two.__eq__(three) and three.__eq__(four):
             return True
-
-    if(third_list[ 0] >=0 and first_list[0] <7 and first_list[3] >= 0 and first_list[3 < 7]):
-        one = grid[last_row][first_list[0]]
-        two = grid[last_row][first_list[1]]
-        three = grid[last_row][first_list[2]]
-        four = grid[last_row][first_list[3]]
+    if (third_list[0] >= 0 and third_list[0] < 7 and third_list[3] >= 0 and third_list[3] < 7):
+        one = grid[last_row][third_list[0]]
+        two = grid[last_row][third_list[1]]
+        three = grid[last_row][third_list[2]]
+        four = grid[last_row][third_list[3]]
         if one.__eq__(two) and two.__eq__(three) and three.__eq__(four):
-            return True 
-
-    if(fourth_list[ 0] >=0 and first_list[0] <7 and first_list[3] >= 0 and first_list[3 < 7]):
-        one = grid[last_row][first_list[0]]
-        two = grid[last_row][first_list[1]]
-        three = grid[last_row][first_list[2]]
-        four = grid[last_row][first_list[3]]
+            return True
+    if (fourth_list[0] >= 0 and fourth_list[0] < 7 and fourth_list[3] >= 0 and fourth_list[3] < 7):
+        one = grid[last_row][fourth_list[0]]
+        two = grid[last_row][fourth_list[1]]
+        three = grid[last_row][fourth_list[2]]
+        four = grid[last_row][fourth_list[3]]
         if one.__eq__(two) and two.__eq__(three) and three.__eq__(four):
-            return True               
-
+            return True
+    return False
+        
 def check_col():
     first_list = [last_row, last_row + 1, last_row + 2, last_row + 3]
     second_list = [last_row - 1, last_row, last_row , last_row + 2]
@@ -139,7 +138,7 @@ def check_col():
         if one.__eq__(two) and two.__eq__(three) and three.__eq__(four):
             return True
 
-    if(second_list[ 0] >=0 and first_list[0] <6 and first_list[3] >= 0 and first_list[3 < 6]):
+    if(second_list[ 0] >=0 and second_list[0] <6 and second_list[3] >= 0 and second_list[3] < 6):
         one = grid[second_list[0]][last_col]
         two = grid[second_list[1]][last_col]
         three = grid[second_list[2]][last_col]
@@ -147,28 +146,28 @@ def check_col():
         if one.__eq__(two) and two.__eq__(three) and three.__eq__(four):
             return True
 
-    if(third_list[ 0] >=0 and first_list[0] <6 and first_list[3] >= 0 and first_list[3 < 6]):
-        one = grid[first_list[0]][last_col]
-        two = grid[first_list[1]][last_col]
-        three = grid[first_list[2]][last_col]
-        four = grid[first_list[3]][last_col]
+    if(third_list[ 0] >=0 and third_list[0] <6 and third_list[3] >= 0 and third_list[3] < 6):
+        one = grid[third_list[0]][last_col]
+        two = grid[third_list[1]][last_col]
+        three = grid[third_list[2]][last_col]
+        four = grid[third_list[3]][last_col]
         if one.__eq__(two) and two.__eq__(three) and three.__eq__(four):
             return True 
-
-    if(fourth_list[ 0] >=0 and first_list[0] <6 and first_list[3] >= 0 and first_list[3 < 6]):
-        one = grid[first_list[0]][last_col]
-        two = grid[first_list[1]][last_col]
-        three = grid[first_list[2]][last_col]
-        four = grid[first_list[3]][last_col]
+  
+    if(fourth_list[ 0] >=0 and fourth_list[0] <6 and fourth_list[3] >= 0 and fourth_list[3] < 6):
+        one = grid[fourth_list[0]][last_col]
+        two = grid[fourth_list[1]][last_col]
+        three = grid[fourth_list[2]][last_col]
+        four = grid[fourth_list[3]][last_col]
         if one.__eq__(two) and two.__eq__(three) and three.__eq__(four):
             return True
     return False
 
 def check_left_diag():
-    first_list_row = [last_col - 1, last_col, last_col + 1, last_col + 2]
+    first_list_row = [last_col, last_col + 1, last_col + 2, last_col + 3]
     first_list_col = [last_row, last_row + 1, last_row + 2, last_row + 3]
-    second_list_row = [last_col - 1, last_col, last_col , last_col + 2]
-    second_list_col = [last_row - 1, last_row, last_row , last_row + 2]
+    second_list_row = [last_col - 1, last_col, last_col + 1, last_col + 2]
+    second_list_col = [last_row - 1, last_row, last_row + 1, last_row + 2]
     third_list_row = [last_col - 2, last_col - 1, last_col , last_col + 1]
     third_list_col = [last_row - 2, last_row - 1, last_row , last_row + 1]
     fourth_list_row = [last_col - 3, last_col - 2, last_col - 1 , last_col]
@@ -181,8 +180,8 @@ def check_left_diag():
             four = grid[first_list_col[3]][first_list_row[3]]
             if one.__eq__(two) and two.__eq__(three) and three.__eq__(four):
                 return True
-    if (second_list_col[0] >= 0 and second_list_col[0] < 7 and second_list_col[3] >= 0 and second_list_col[3] < 7):
-        if(second_list_row[0] >= 0 and second_list_row[0] < 6 and second_list_row[3] >= 0 and second_list_row[3] < 6):
+    if (second_list_col[0] >= 0 and second_list_col[0] < 6 and second_list_col[3] >= 6 and second_list_col[3] < 6):
+        if(second_list_row[0] >= 0 and second_list_row[0] < 7 and second_list_row[3] >= 0 and second_list_row[3] < 7):
             one = grid[second_list_col[0]][second_list_row[0]]
             two = grid[second_list_col[1]][second_list_row[1]]
             three = grid[second_list_col[2]][second_list_row[2]]
@@ -190,73 +189,72 @@ def check_left_diag():
             if one.__eq__(two) and two.__eq__(three) and three.__eq__(four):
                 return True
     
-    if(third_list_row[ 0] >=0 and third_list_row[0] <7 and second_list_row[3] >= 0 and third_list_row[3 < 7]):
-        if(third_list_col[ 0] >=0 and third_list_col[0] <6 and third_list_col[3] >= 0 and third_list_col[3 < 6]):
+    if(third_list_row[ 0] >=0 and third_list_row[0] <7 and third_list_row[3] >= 0 and third_list_row[3] < 7):
+        if(third_list_col[ 0] >=0 and third_list_col[0] <6 and third_list_col[3] >= 0 and third_list_col[3] < 6):
             one = grid[third_list_col[0]][third_list_row[0]]
             two = grid[third_list_col[1]][third_list_row[1]]
             three = grid[third_list_col[2]][third_list_row[2]]
             four = grid[third_list_col[3]][third_list_row[3]]
             if one.__eq__(two) and two.__eq__(three) and three.__eq__(four):
                 return True
-    if(fourth_list_col[ 0] >=0 and second_list_col[0] <6 and second_list_col[3] >= 0 and second_list_col[3 < 6]):
-        one = grid[fourth_list_col[0]][fourth_list_col[0]]
-        two = grid[fourth_list_col[1]][fourth_list_col[1]]
-        three = grid[fourth_list_col[2]][fourth_list_col[2]]
-        four = grid[fourth_list_col[3]][fourth_list_col[3]]
-        if one.__eq__(two) and two.__eq__(three) and three.__eq__(four):
-            return True
+    if(fourth_list_row[ 0] >=0 and fourth_list_row[0] <7 and fourth_list_row[3] >= 0 and fourth_list_row[3] < 7):
+        if(fourth_list_col[ 0] >=0 and fourth_list_col[0] <6 and fourth_list_col[3] >= 0 and fourth_list_col[3] < 6):
+            one = grid[fourth_list_col[0]][fourth_list_row[0]]
+            two = grid[fourth_list_col[1]][fourth_list_row[1]]
+            three = grid[fourth_list_col[2]][fourth_list_row[2]]
+            four = grid[fourth_list_col[3]][fourth_list_row[3]]
+            if one.__eq__(two) and two.__eq__(three) and three.__eq__(four):
+                return True
+    return False
 
 def check_right_diag():
-    first_list_row = [last_row, last_row + 1, last_row + 2, last_row + 3]
-    first_list_col = [last_col + 1, last_col, last_col + 1, last_col + 2]
-    second_list_row = [last_col - 1, last_col, last_col , last_col + 2]
-    second_list_col = [last_row + 1, last_row, last_row , last_row + 2]
-    third_list_row = [last_col - 2, last_col - 1, last_col , last_col + 1]
+    first_list_row = [last_col, last_col - 1, last_col - 2, last_col - 3]
+    first_list_col = [last_row, last_row + 1, last_row + 2, last_row + 3]
+    second_list_row = [last_col + 1, last_col, last_col - 1, last_col - 2]
+    second_list_col = [last_row - 1, last_row, last_row + 1, last_row + 2]
+    third_list_row = [last_col + 2, last_col + 1, last_col , last_col - 1]
     third_list_col = [last_row - 2, last_row - 1, last_row , last_row + 1]
-    fourth_list_row = [last_col - 3, last_col - 2, last_col - 1 , last_col]
+    fourth_list_row = [last_col + 3, last_col + 2, last_col + 1 , last_col]
     fourth_list_col = [last_row - 3, last_row - 2, last_row - 1 , last_row]
     if(first_list_row[0] >= 0 and first_list_row[0] < 7 and first_list_row[3] >= 0 and first_list_row[3] < 7):
         if(first_list_col[0] >= 0 and first_list_col[0] < 6 and first_list_col[3] >= 0 and first_list_col[3] < 6):
             one = grid[first_list_col[0]][first_list_row[0]]
-            two = grid[first_list_col[3]][first_list_row[1]]
-            three = grid[first_list_col[1]][first_list_row[2]]
-            four = grid[first_list_col[2]][first_list_row[3]]
+            two = grid[first_list_col[1]][first_list_row[1]]
+            three = grid[first_list_col[2]][first_list_row[2]]
+            four = grid[first_list_col[3]][first_list_row[3]]
             if one.__eq__(two) and two.__eq__(three) and three.__eq__(four):
                 return True
-    if (second_list_col[0] >= 0 and second_list_col[0] < 7 and second_list_col[3] >= 0 and second_list_col[3] < 7):
-        if(second_list_row[0] >= 0 and second_list_row[0] < 6 and second_list_row[3] >= 0 and second_list_row[3] < 6):
+    if (second_list_col[0] >= 0 and second_list_col[0] < 6 and second_list_col[3] >= 0 and second_list_col[3] < 6):
+        if(second_list_row[0] >= 0 and second_list_row[0] < 7 and second_list_row[3] >= 0 and second_list_row[3] < 7):
             one = grid[second_list_col[0]][second_list_row[0]]
             two = grid[second_list_col[1]][second_list_row[1]]
             three = grid[second_list_col[2]][second_list_row[2]]
             four = grid[second_list_col[3]][second_list_row[3]]
             if one.__eq__(two) and two.__eq__(three) and three.__eq__(four):
                 return True
-    if(third_list_row[ 0] >=0 and third_list_row[0] <7 and second_list_row[3] >= 0 and third_list_row[3 < 7]):
-        if(third_list_col[ 0] >=0 and third_list_col[0] <6 and third_list_col[3] >= 0 and third_list_col[3 < 6]):
+    
+    if(third_list_row[ 0] >=0 and third_list_row[0] <7 and third_list_row[3] >= 0 and third_list_row[3] < 7):
+        if(third_list_col[ 0] >=0 and third_list_col[0] <6 and third_list_col[3] >= 0 and third_list_col[3] < 6):
             one = grid[third_list_col[0]][third_list_row[0]]
             two = grid[third_list_col[1]][third_list_row[1]]
             three = grid[third_list_col[2]][third_list_row[2]]
             four = grid[third_list_col[3]][third_list_row[3]]
             if one.__eq__(two) and two.__eq__(three) and three.__eq__(four):
                 return True
-    if(fourth_list_col[ 0] >=0 and second_list_col[0] <6 and second_list_col[3] >= 0 and second_list_col[3 < 6]):
-        one = grid[fourth_list_col[0]][fourth_list_col[0]]
-        two = grid[fourth_list_col[1]][fourth_list_col[1]]
-        three = grid[fourth_list_col[2]][fourth_list_col[2]]
-        four = grid[fourth_list_col[3]][fourth_list_col[3]]
-        if one.__eq__(two) and two.__eq__(three) and three.__eq__(four):
-            return True
+    if(fourth_list_row[ 0] >=0 and fourth_list_row[0] <7 and fourth_list_row[3] >= 0 and fourth_list_row[3] < 7):
+        if(fourth_list_col[ 0] >=0 and fourth_list_col[0] <6 and fourth_list_col[3] >= 0 and fourth_list_col[3] < 6):
+            one = grid[fourth_list_col[0]][fourth_list_row[0]]
+            two = grid[fourth_list_col[1]][fourth_list_row[1]]
+            three = grid[fourth_list_col[2]][fourth_list_row[2]]
+            four = grid[fourth_list_col[3]][fourth_list_row[3]]
+            if one.__eq__(two) and two.__eq__(three) and three.__eq__(four):
+                return True
+    return False
 
-    
-    
-    
 
 def check_draw():
-    for row in range(6):
-        for col in range(6):
-            if grid[row][col].isnumeric():
-                return False  
-    return True
+    return remaining_spots == 0
+
 
 def check_game_over():
     if check_row():
@@ -276,30 +274,12 @@ def check_game_over():
         return True
     else:
         return False
-
-def game_loop():
-    global current_piece
-    print("Welcome to CONNECT FOUR")
-    user_choice = ""
-    draw_grid()
-    while (True):
-        while (is_bad_choice(user_choice)):
-            user_choice = input(
-                "Enter STOP to end.  Or a number (0-6) where to drop the piece: ")
-        if user_choice.__eq__("STOP"):
-            break
-        column = int(user_choice)
-        place_piece(column)
-        if (check_game_over()):
-            break
-        current_piece = "Y" if current_piece.__eq__("R") else "R"
-        user_choice = ""
-
+    
 def handle_click(e):
     global is_game_over
     global current_piece
     global current_col_choice
-    global place_piece
+    global piece_placed
     mouse_x = e.x
     if mouse_x >= 0 * col_width and mouse_x < 1 * col_width:
         current_col_choice = 0
@@ -308,27 +288,28 @@ def handle_click(e):
     elif mouse_x >= 2 * col_width and mouse_x < 3 * col_width:
         current_col_choice = 2
     elif mouse_x >= 3 * col_width and mouse_x < 4 * col_width:
-        current_col_choice = 3
+        current_col_choice = 3        
     elif mouse_x >= 4 * col_width and mouse_x < 5 * col_width:
         current_col_choice = 4
     elif mouse_x >= 5 * col_width and mouse_x < 6 * col_width:
+        print(mouse_x)
         current_col_choice = 5
     elif mouse_x >= 6 * col_width and mouse_x < 7 * col_width:
         current_col_choice = 6
     if(not is_game_over):
         place_piece()
-    if(not is_game_over and check_game_over()):
+    if (not is_game_over and check_game_over()):
         is_game_over = True
-    if(place_piece):
+    if(piece_placed):
         current_piece = "Y" if current_piece.__eq__("R") else "R"
-    pass
 
-def print_info():
+def print_intro():
     print("Welcome to CONNECT FOUR")
     draw_grid()
 
+
 screen.bind("<Button-1>", handle_click)
-        
+
 print_intro()
 
 mainloop()
---
+++ 13_FINAL PROJECT/day_3_.py
@@ -0,0 +1,147 @@
+import tkinter as tk
+
+class Game(tk.Frame):
+    def __init__(self, master):
+        super(Game, self).__init__(master)
+        self.lives = 3
+        self.width = 600
+        self.height = 400
+        self.bg = "#AAAAFF"
+        self.canvas = tk.Canvas(self, width=self.width, height=self.height, bg=self.bg)        
+        self.canvas.pack()
+        self.pack()
+
+        self.items = {}
+        self.ball = None
+        self.paddle = Paddle(self.canvas, self.width / 2, 326)
+        self.items[self.paddle.item] = self.paddle
+
+        for x in range(5, self.width - 75, 75):
+            self.add_brick(x + 37.5, 50, 2)
+            self.add_brick(x + 37.5, 70, 1)
+            self.add_brick(x + 37.5, 90, 1)
+
+        self.hud = None
+        self.setup_game()
+        self.canvas.focus_set()
+        self.canvas.bind("<Left>", lambda _: self.paddle.move(-10))
+        self.canvas.bind("<Right>", lambda _: self.paddle.move(10))
+
+    def setup_game(self):
+        self.add_ball()
+        self.update_lives_text()
+        self.text = self.draw_text(300, 200, "Press Space to start")
+        self.canvas.bind("<space>", lambda _: self.start_game())
+
+    def add_ball(self):
+        if self.ball is not None:
+            self.ball.delete()
+        paddle_coords = self.paddle.get_position()
+        x = (paddle_coords[0] + paddle_coords[2]) / 2
+        self.ball = Ball(self.canvas, x, 310)
+        self.paddle.set_ball(self.ball)
+
+    def add_brick(self, x, y, hits):
+        brick = Brick(self.canvas, x, y, hits)
+        self.items[brick.item] = brick
+
+    def draw_text(self, x, y, text, size="40"):
+        font = ("Helvetica", size)
+        return self.canvas.create_text(x, y, text=text, font=font)
+    
+    def update_lives_text(self):
+        text = "Lives: %s" % self.lives
+        if self.hud == None:
+            self.hud = self.draw_text(50, 20, text, 15)
+        else:
+            self.canvas.itemconfig(self.hud, text=text)
+
+    def start_game(self):
+        pass
+
+    
+
+
+class GameObject(object):
+    def __init__(self, canvas, item):
+        self.canvas = canvas
+        self.item = item
+
+    def get_position(self):
+        return self.canvas.coords(self.item)
+    
+    def move(self, x, y):
+        self.canvas.move(self.item, x, y)
+
+    def delete(self):
+        self.canvas.delete(self.item)
+
+class Ball(GameObject):
+    def __init__(self, canvas, x, y): 
+        self.radius = 10
+        self.direction = [1, -1]
+        self.speed = 10
+        x1 = x - self.radius
+        y1 = y - self.radius
+        x2 = x + self.radius
+        y2 = y + self.radius  
+        color = "white"
+        item = canvas.create_oval(x1, y1, x2, y2, fill=color)
+        super(Ball, self).__init__(canvas, item)
+
+class Paddle(GameObject):
+    def __init__(self, canvas, x, y):  
+        self.width = 80
+        self.height = 10
+        self.ball = None
+        x1 = x - self.width / 2 
+        y1 = y - self.height / 2
+        x2 = x + self.width / 2
+        y2 = y + self.height / 2
+        color = "blue"
+        item = canvas.create_rectangle(x1, y1, x2, y2, fill=color)
+        super(Paddle, self).__init__(canvas, item)
+
+    def set_ball(self, ball):
+        self.ball = ball
+
+    def move(self, offset):
+        coords = self.get_position()
+        width = self.canvas.winfo.width()
+        x1 = coords[0]
+        y1 = coords[1]
+        x2 = coords[2]
+        y2 = coords[3]
+        if x1 + offset >= 0 and x2 + offset <= width:
+            super(Paddle, self).move(offset, 0)
+        if self.ball is not None:
+            self.ball.move(offset, 0)
+
+class Brick(GameObject):
+    COLORS = {1 : "#999999", 2 : "#555555", 3 : "#222222"}
+
+    def __init__(self, canvas, x, y, hits):
+        self.width = 75
+        self.height = 20
+        self.hits = hits 
+        color = Brick.COLORS[hits]
+        x1 = x - self.width / 2
+        y1 = y - self.height / 2
+        x2 = x + self.width / 2
+        y2 = y + self.height / 2
+        item = canvas.create_rectangle(x1, y1, x2, y2, fill=color, tags="brick")        
+        super(Brick, self).__init__(canvas, item)
+
+    def hit(self):
+        self.hits -= 1
+        self.hits == 0.  #If it is call self.delete().  If not 
+        self.canvas.itemconfig(self.item, fill=Brick.COLORS[self.hits])
+
+
+
+
+if __name__ == "__main__":    
+    root = tk.Tk()
+    game = Game(root)
+    root.title("BREAKOUT")
+    root.mainloop()
